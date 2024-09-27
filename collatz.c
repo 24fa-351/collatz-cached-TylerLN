2362767b04b5771f4dbcdc7ed7a6bffec865cd60
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-27 12:21:09 -0700

    Added random numbers and steps to csv file

--- collatz.c
+++ collatz.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 unsigned long long int collatz_steps(unsigned long long int n) {
-    unsigned long long int steps = 0;
-    while (n != 1) {
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else if (n % 2 == 1) {
-            n = 3 * n + 1;
-        }
-        steps++;
+  unsigned long long int steps = 0;
+  while (n != 1) {
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
     }
-    return steps;
-}
-
-unsigned long long int Randomnizer(unsigned long long int MAX, unsigned long long int MIN) {
-    unsigned long long int rand_num = rand() % (MAX - MIN + 1) + MIN;
-    
-    return rand_num;
-
+    steps++;
+  }
+  return steps;
 }
 
 int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    printf("Usage: %s <num_values> <min_value> <max_value>\n", argv[0]);
+    return 1;
+  }
 
+  int user_n = atoi(argv[1]);
+  int Min = atoi(argv[2]);
+  int Max = atoi(argv[3]);
 
-return 0;
+  FILE *csv_file = fopen("collatz_results.csv", "w");
+  if (csv_file == NULL) {
+    printf("Error opening file!\n");
+    return 1;
+  }
 
+  fprintf(csv_file, "N,Min,Max,Random Number,Steps\n");
+
+  srand(time(NULL));
+
+  for (int ix = 0; ix < user_n; ix++) {
+    int rand_num = rand() % (Max - Min + 1) + Min;
+    unsigned long long int steps = collatz_steps(rand_num);
+
+    fprintf(csv_file, "%d,%d,%d,%d,%llu\n", user_n, Min, Max, rand_num, steps);
+  }
+
+  fclose(csv_file);
+
+  printf("Results written to collatz_results.csv\n");
+
+  return 0;
 }
